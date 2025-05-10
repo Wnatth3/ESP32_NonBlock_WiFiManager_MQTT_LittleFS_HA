ffb9d51f26a2c3a5413dd820018ff477b5fa3dc2
Phattharakorn Mahasorasak <foolproof.ja@gmail.com>
2025-05-10 12:12:10 +0700

    Remove #ifdef _DEBUG_

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,18 @@
 #include <TickTwo.h>
 
 //******************************** Configulation ****************************//
-#define FORMAT_LITTLEFS_IF_FAILED true
-
 #define _DEBUG_  // Comment this line if you don't want to debug
+#include "Debug.h"
+
+// #define CUSTOM_IP // Uncomment this line if you want to use DHCP
+
 // #define _RemoveEntity  // Uncomment this line if you want to remove the entity from Home Assistant
 
 //******************************** Variables & Objects **********************//
+#define FORMAT_LITTLEFS_IF_FAILED true
+
 #define deviceName "MyESP32"
 
-const char* filename = "/config.txt";  // Config file name
-
-bool mqttParameter;
 //----------------- esLED ---------------------//
 #define ledPin     LED_BUILTIN
 #define airPumpPin 4
@@ -47,10 +48,21 @@ Preferences pf;
 Button2 resetWifiBt;
 
 //----------------- WiFi Manager --------------//
+const char* filename = "/config.txt";  // Config file name
+
+#ifdef CUSTOM_IP
+// default custom static IP
+char static_ip[16]  = "192.168.0.191";
+char static_gw[16]  = "192.168.0.1";
+char static_sn[16]  = "255.255.255.0";
+char static_dns[16] = "1.1.1.1";
+#endif
 char mqttBroker[16] = "192.168.0.10";
 char mqttPort[6]    = "1883";
 char mqttUser[10];
 char mqttPass[10];
+
+bool mqttParameter;
 
 WiFiManager wifiManager;
 
@@ -93,12 +105,13 @@ TickTwo tReadSendData(readSendData, 1000, 0, MILLIS);  // (function, interval, i
 //----------------- LittleFS ------------------//
 // Loads the configuration from a file
 void loadConfiguration(fs::FS& fs, const char* filename) {
+    _delnF("Loading configuration");
     // Open file for reading
     File file = fs.open(filename, "r");
     if (!file) {
-#ifdef _DEBUG_
+
         Serial.println(F("Failed to open data file"));
-#endif
+
         return;
     }
 
@@ -107,9 +120,9 @@ void loadConfiguration(fs::FS& fs, const char* filename) {
     // Deserialize the JSON document
     DeserializationError error = deserializeJson(doc, file);
     if (error) {
-#ifdef _DEBUG_
+
         Serial.println(F("Failed to read file, using default configuration"));
-#endif
+
     }
     // Copy values from the JsonDocument to the Config
     // strlcpy(Destination_Variable, doc["Source_Variable"] /*| "Default_Value"*/, sizeof(Destination_Name));
@@ -119,17 +132,28 @@ void loadConfiguration(fs::FS& fs, const char* filename) {
     strlcpy(mqttPass, doc["mqttPass"], sizeof(mqttPass));
     mqttParameter = doc["mqttParameter"];
 
+#ifdef CUSTOM_IP
+    if (doc["ip"]) {
+        strlcpy(static_ip, doc["ip"], sizeof(static_ip));
+        strlcpy(static_gw, doc["gateway"], sizeof(static_gw));
+        strlcpy(static_sn, doc["subnet"], sizeof(static_sn));
+        strlcpy(static_dns, doc["dns"], sizeof(static_dns));
+    } else {
+        _delnF("No custom IP in config file");
+    }
+#endif
+
     file.close();
 }
 
 void saveState(bool var, const char* key, bool state) {
     var = state;
     pf.putBool(key, state);
-#ifdef _DEBUG_
+
     Serial.print(key);
     Serial.print(F(": "));
     Serial.println(pf.getBool(kAirPumpSt));
-#endif
+
 }
 
 void handleMqttMessage(char* topic, byte* payload, unsigned int length) {
@@ -150,21 +174,21 @@ void handleMqttMessage(char* topic, byte* payload, unsigned int length) {
 }
 
 void mqttInit() {
-#ifdef _DEBUG_
+
     Serial.print(F("MQTT parameters are "));
-#endif
+
     if (mqttParameter) {
-#ifdef _DEBUG_
+
         Serial.println(F(" available"));
-#endif
+
         mqtt.setBufferSize(512);
         mqtt.setCallback(handleMqttMessage);
         mqtt.setServer(mqttBroker, atoi(mqttPort));
         tConnectMqtt.start();
     } else {
-#ifdef _DEBUG_
+
         Serial.println(F(" not available."));
-#endif
+
     }
 }
 
@@ -174,17 +198,17 @@ void saveParamsCallback() {
     strcpy(mqttPort, customMqttPort.getValue());
     strcpy(mqttUser, customMqttUser.getValue());
     strcpy(mqttPass, customMqttPass.getValue());
-#ifdef _DEBUG_
+
     Serial.println(F("The values are updated."));
-#endif
+
 
     // Delete existing file, otherwise the configuration is appended to the file
     // LittleFS.remove(filename);
     File file = LittleFS.open(filename, "w");
     if (!file) {
-#ifdef _DEBUG_
+
         Serial.println(F("Failed to open config file for writing"));
-#endif
+
         return;
     }
 
@@ -195,107 +219,114 @@ void saveParamsCallback() {
     doc["mqttPort"]   = mqttPort;
     doc["mqttUser"]   = mqttUser;
     doc["mqttPass"]   = mqttPass;
-#ifdef _DEBUG_
+
     Serial.print(F("The configuration has been saved to "));
     Serial.println(filename);
-#endif
+
 
     if (doc["mqttBroker"] != "") {
         doc["mqttParameter"] = true;
         mqttParameter        = doc["mqttParameter"];
     }
-
+#ifdef CUSTOM_IP
+    doc["ip"]      = WiFi.localIP().toString();
+    doc["gateway"] = WiFi.gatewayIP().toString();
+    doc["subnet"]  = WiFi.subnetMask().toString();
+    doc["dns"]     = WiFi.dnsIP().toString();
+#endif
     // Serialize JSON to file
     if (serializeJson(doc, file) == 0) {
-#ifdef _DEBUG_
-        Serial.println(F("Failed to write to file"));
-#endif
+        _delnF("Failed to write to file");
+    } else {
+        _delnF("Configuration saved successfully");
     }
 
     file.close();  // Close the file
-#ifdef _DEBUG_
+
     Serial.println(F("Configuration saved"));
-#endif
+
 
     mqttInit();
 }
 
-// Prints the content of a file to the Serial
 void printFile(fs::FS& fs, const char* filename) {
-    // Open file for reading
+    _delnF("Print config file...");
     File file = fs.open(filename, "r");
     if (!file) {
-#ifdef _DEBUG_
-        Serial.println(F("Failed to open data file"));
-#endif
+        _delnF("Failed to open data file");
         return;
     }
 
-    // Extract each characters by one by one
-    while (file.available()) {
-        Serial.print((char)file.read());
+    JsonDocument         doc;
+    DeserializationError error = deserializeJson(doc, file);
+    if (error) {
+        _delnF("Failed to read file");
     }
-    Serial.println();
 
-    file.close();  // Close the file
+    char buffer[512];
+    serializeJsonPretty(doc, buffer);
+    _delnF(buffer);
+
+    file.close();
 }
 
 void deleteFile(fs::FS& fs, const char* path) {
-#ifdef _DEBUG_
-    Serial.print(F("Deleting file: "));
-    Serial.println(String(path) + "\r\n");
-#endif
+    _deVarln("Delete file: ", path);
     if (fs.remove(path)) {
-#ifdef _DEBUG_
-        Serial.println(F("- file deleted"));
-#endif
+        _delnF("- file deleted");
     } else {
-#ifdef _DEBUG_
-        Serial.println(F("- delete failed"));
-#endif
+        _delnF("- delete failed");
     }
 }
 
 //----------------- Wifi Manager --------------//
 void wifiManagerSetup() {
-#ifdef _DEBUG_
-    Serial.println(F("Loading configuration..."));
-#endif
     loadConfiguration(LittleFS, filename);
+#ifdef _DEBUG_
+    printFile(LittleFS, filename);
+#endif
 
-    // add all your parameters here
+    // reset settings - wipe credentials for testing
+    // wifiManager.resetSettings();
+
+#ifdef CUSTOM_IP
+    // set static ip
+    IPAddress _ip, _gw, _sn, _dns;
+    _ip.fromString(static_ip);
+    _gw.fromString(static_gw);
+    _sn.fromString(static_sn);
+    _dns.fromString(static_dns);
+    wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn, _dns);
+#endif
+
     wifiManager.addParameter(&customMqttBroker);
     wifiManager.addParameter(&customMqttPort);
     wifiManager.addParameter(&customMqttUser);
     wifiManager.addParameter(&customMqttPass);
 
     wifiManager.setDarkMode(true);
+#ifndef _DEBUG_
+    wifiManager.setDebugOutput(true, WM_DEBUG_SILENT);
+#endif
+    // wifiManager.setDebugOutput(true, WM_DEBUG_DEV);
+    // wifiManager.setMinimumSignalQuality(20); // Default: 8%
     // wifiManager.setConfigPortalTimeout(60);
     wifiManager.setConfigPortalBlocking(false);
-#ifdef _DEBUG_
-    Serial.println(F("Saving configuration..."));
-#endif
     wifiManager.setSaveParamsCallback(saveParamsCallback);
-#ifdef _DEBUG_
-    Serial.println(F("Print config file..."));
-#endif
-    printFile(LittleFS, filename);
 
+    // automatically connect using saved credentials if they exist
+    // If connection fails it starts an access point with the specified name
     if (wifiManager.autoConnect(deviceName, "password")) {
-#ifdef _DEBUG_
-        Serial.println(F("WiFI is connected :D"));
-#endif
+        _delnF("WiFI is connected :D");
     } else {
-#ifdef _DEBUG_
-        Serial.println(F("Configportal running"));
-#endif
+        _delnF("Configportal running");
     }
 }
 
 void subscribeMqtt() {
-#ifdef _DEBUG_
+
     Serial.println(F("Subscribing to the MQTT topics..."));
-#endif
+
     mqtt.subscribe(commandTopicAirPumpSw);
 }
 //----------------- Add MQTT Entities to HA ---//
@@ -305,9 +336,9 @@ void addMqttEntities() {
     char airPupmSwBuff[512];
 
     JsonDocument doc;
-#ifdef _DEBUG_
+
     Serial.println(F("Adding the DHT22Temp entity"));
-#endif
+
     doc.clear();
     doc["name"]                = "Temp";
     doc["unique_id"]           = "dht22Temp";
@@ -325,9 +356,9 @@ void addMqttEntities() {
     mqtt.publish(configTopicDht22Humi, "", true);
 #endif
 
-#ifdef _DEBUG_
+
     Serial.println(F("Adding the DHT22Humi entity"));
-#endif
+
     doc.clear();
     doc["name"]                = "Humi";
     doc["unique_id"]           = "dht22Humi";
@@ -345,9 +376,9 @@ void addMqttEntities() {
     mqtt.publish(configTopicDht22Humi, "", true);
 #endif
 
-#ifdef _DEBUG_
+
     Serial.println(F("Adding the airPumpSw entity"));
-#endif
+
     doc.clear();
     doc["name"]               = "Switch";
     doc["unique_id"]          = "airPumpSw";
@@ -378,14 +409,14 @@ void publishMqtt() {
 //----------------- Connect MQTT --------------//
 void reconnectMqtt() {
     if (WiFi.status() == WL_CONNECTED) {
-#ifdef _DEBUG_
-        Serial.println(F("Connecting MQTT... "));
-#endif
+        _deVar("MQTT Broker: ", mqttBroker);
+        _deVar(" | Port: ", mqttPort);
+        _deVar(" | User: ", mqttUser);
+        _deVarln(" | Pass: ", mqttPass);
+        _deF("Connecting MQTT... ");
         if (mqtt.connect(deviceName, mqttUser, mqttPass)) {
             tReconnectMqtt.stop();
-#ifdef _DEBUG_
-            Serial.println(F("connected"));
-#endif
+            _delnF("Connected");
             tConnectMqtt.interval(0);
             tConnectMqtt.start();
             statusLed.blinkNumberOfTimes(200, 200, 3);  // 200ms ON, 200ms OFF, repeat 3 times, blink immediately
@@ -393,12 +424,12 @@ void reconnectMqtt() {
             addMqttEntities();
             publishMqtt();
         } else {
-#ifdef _DEBUG_
+
             Serial.print(F("failed state: "));
             Serial.println(mqtt.state());
             Serial.print(F("counter: "));
             Serial.println(tReconnectMqtt.counter());
-#endif
+
             if (tReconnectMqtt.counter() >= 3) {
                 tReconnectMqtt.stop();
                 tConnectMqtt.interval(60 * 1000);  // Wait 1 minute before reconnecting.
@@ -407,9 +438,9 @@ void reconnectMqtt() {
         }
     } else {
         if (tReconnectMqtt.counter() <= 1) {
-#ifdef _DEBUG_
+
             Serial.println(F("WiFi is not connected"));
-#endif
+
         }
     }
 }
@@ -426,17 +457,13 @@ void connectMqtt() {
 //----------------- Reset WiFi Button ---------//
 void resetWifiBtPressed(Button2& btn) {
     statusLed.turnON();
-#ifdef _DEBUG_
-    Serial.println(F("Deleting the config file and resetting WiFi."));
-#endif
+    _delnF("Deleting the config file and resetting WiFi.");
     deleteFile(LittleFS, filename);
     wifiManager.resetSettings();
-    Serial.print(deviceName);
-#ifdef _DEBUG_
-    Serial.println(F(" is restarting."));
-#endif
-    ESP.restart();
+    _deF(deviceName);
+    _delnF(" is restarting.");
     delay(3000);
+    ESP.restart();
 }
 
 void toggleTestLed(Button2& btn) {
@@ -452,39 +479,38 @@ void toggleTestLed(Button2& btn) {
 
 void updateAirPumpState() {
     airPumpState = pf.getBool(kAirPumpSt, false);
-#ifdef _DEBUG_
+
     Serial.print(kAirPumpSt);
     Serial.print(F(": "));
     Serial.println(airPumpState);
-#endif
+
     airPumpState ? airPump.turnON() : airPump.turnOFF();
 }
 
 void readSendData() {
     temp = dht.readTemperature();
     humi = dht.readHumidity();
-#ifdef _DEBUG_
+
     Serial.print(F("Temp: "));
     Serial.print(temp);
     Serial.print(F(" °C, Humi: "));
     Serial.print(humi);
     Serial.println(F(" %"));
-#endif
+
     mqtt.publish(stateTopicDht22Temp, String(temp).c_str());
     mqtt.publish(stateTopicDht22Humi, String(humi).c_str());
 }
 
 //********************************  Setup ***********************************//
 void setup() {
-    Serial.begin(115200);
+    _serialBegin(115200);
     pf.begin("myPrefs", false);
-    // Initialize LittleFS library
+
     while (!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)) {
-#ifdef _DEBUG_
-        Serial.println(F("Failed to initialize LittleFS library"));
-#endif
+        _delnF("Failed to initialize LittleFS library");
         delay(1000);
     }
+
     statusLed.turnOFF();
     updateAirPumpState();
     resetWifiBt.begin(resetWifiBtPin);
